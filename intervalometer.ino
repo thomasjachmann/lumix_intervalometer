592d7daeee8b055a461510d1517d2e720424fc4a
Thomas Jachmann <self@thomasjachmann.com>
2012-06-08 00:06:23 +0200

    Reorganize code for better readability
    Input/output pin definitions are in one place now, and other constants
    are nearer to the top of the file as well.

--- intervalometer.ino
+++ intervalometer.ino
@@ -3,15 +3,53 @@
   Triggers shutter via remote control port (coupled via optocouplers).
  */
 
+/***** BEGIN IO initialization *****/
+
+const int inSwitch    = 12; // push button to change the running state with
+const int outRunning  = 11; // LED to display the running state (on when running)
+const int outAction   = 13; // LED to display the action state (on when triggering)
+const int outFocus    = 10; // optocoupler to trigger focus
+const int outShutter  = 9;  // optocoupler to trigger shutter
+                            // pins 0-7 are used as input switches for timing
+
+void setAsInput(int pin) {
+  pinMode(pin, INPUT);
+  digitalWrite(pin, HIGH); // turn on Atmega's pullup resistor
+}
+
+void setup() {
+  setAsInput(inSwitch);
+  for (int i = 0; i < 8; i++) {
+    setAsInput(i);
+  }
+  pinMode(outRunning, OUTPUT);
+  pinMode(outAction, OUTPUT);
+  pinMode(outFocus, OUTPUT);
+  pinMode(outShutter, OUTPUT);
+}
+
+/***** END IO initialization *****/
+
+
+/***** BEGIN loop *****/
+
 unsigned long curTime = 0;
 unsigned long nextActiveChangeTime = 0;
 const int actionDuration = 500;
 
-/***** BEGIN switch to choose between running/sleeping *****/
-boolean running = false;      // are we intervalometering?
-const int outRunning = 11;    // LED to display the running state
+void loop() {
+  curTime = millis();
+  checkRunning();
+  performAction();
+}
 
-const int inSwitch = 12;      // push button to change the running state with
+/***** END loop *****/
+
+
+/***** BEGIN switch to choose between running/sleeping *****/
+
+boolean running = false;      // are we intervalometering?
+
 int prevSwitch = HIGH;
 int debounceSwitch = HIGH;
 unsigned long prevSwitchDebounceTime = 0;
@@ -36,9 +74,12 @@ void checkRunning() {
     prevSwitch = curSwitch;
   }
 }
-/***** END *****/
 
-/***** BEGIN interval determination *****/
+/***** END running switch *****/
+
+
+/***** BEGIN action stuff *****/
+
 unsigned long actionInterval() {
   unsigned long interval = 0;
   for (int i = 0; i < 8; i++) {
@@ -51,13 +92,7 @@ unsigned long actionInterval() {
   }
   return interval;
 }
-/***** END *****/
-
-/***** BEGIN action stuff *****/
 boolean active = false;       // are we executing the action?
-const int outAction = 13;     // LED to display the action state
-const int outFocus = 10;      // optocoupler to trigger focus
-const int outShutter = 9;     // optocoupler to trigger shutter
 
 void performAction() {
   if (curTime >= nextActiveChangeTime) {
@@ -70,26 +105,5 @@ void performAction() {
     nextActiveChangeTime = curTime + (active ? actionDuration : curActionInterval);
   }
 }
-/***** END *****/
 
-void setAsInput(int pin) {
-  pinMode(pin, INPUT);
-  digitalWrite(pin, HIGH); // turn on Atmega's pullup resistor
-}
-
-void setup() {
-  setAsInput(inSwitch);
-  for (int i = 0; i < 8; i++) {
-    setAsInput(i);
-  }
-  pinMode(outRunning, OUTPUT);
-  pinMode(outAction, OUTPUT);
-  pinMode(outFocus, OUTPUT);
-  pinMode(outShutter, OUTPUT);
-}
-
-void loop() {
-  curTime = millis();
-  checkRunning();
-  performAction();
-}
+/***** END action stuff *****/
